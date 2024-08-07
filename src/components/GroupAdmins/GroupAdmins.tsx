e413a4615c6a37669efebefad35161f6bab59a7a
Ritik <160255700+gritik418@users.noreply.github.com>
2024-08-07 10:36:16 +0530

    Theme enabled on group info page

--- src/components/GroupAdmins/GroupAdmins.tsx
+++ src/components/GroupAdmins/GroupAdmins.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import GroupAdminItem from "../GroupAdminItem/GroupAdminItem";
+import { useCustomTheme } from "@/contexts/theme/ThemeProvider";
 
 const GroupAdmins = ({ admins }: { admins: ChatAdminType[] }) => {
+  const { theme } = useCustomTheme();
   return (
-    <div className="container min-h-[40vh] mb-[100px] mx-auto p-4">
-      <div className="bg-slate-100 rounded-lg p-4 flex flex-col">
-        <h2 className="text-2xl mb-4">Group Admins</h2>
+    <div className="container min-h-[40vh] mb-[70px] mx-auto p-4">
+      <div
+        className={`rounded-lg p-4 flex flex-col ${
+          theme === "dark" ? "bg-[#2d2b41]" : "bg-slate-100"
+        }`}
+      >
+        <h2 className={`text-2xl mb-4 ${theme === "dark" ? "text-white" : ""}`}>
+          Group Admins
+        </h2>
         <div className="flex flex-col gap-3 max-h-[500px] overflow-y-scroll">
           {admins &&
             admins.map((admin: ChatAdminType) => {
--- src/components/GroupMembers/GroupMembers.tsx
+++ src/components/GroupMembers/GroupMembers.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import GroupMemberItem from "../GroupMemberItem/GroupMemberItem";
+import { useCustomTheme } from "@/contexts/theme/ThemeProvider";
 
 const GroupMembers = ({
   members,
@@ -8,10 +9,17 @@ const GroupMembers = ({
   members: ChatMemberType[];
   adminIds: string[];
 }) => {
+  const { theme } = useCustomTheme();
   return (
     <div className="container min-h-[40vh] mb-[20px] mx-auto p-4">
-      <div className="bg-slate-100 rounded-lg p-4 flex flex-col">
-        <h2 className="text-2xl mb-4">Group Members</h2>
+      <div
+        className={`rounded-lg p-4 flex flex-col ${
+          theme === "dark" ? "bg-[#2d2b41]" : "bg-slate-100"
+        }`}
+      >
+        <h2 className={`text-2xl mb-4 ${theme === "dark" ? "text-white" : ""}`}>
+          Group Members
+        </h2>
         <div className="flex flex-col gap-3 max-h-[500px] overflow-y-scroll">
           {members &&
             members.map((member: ChatMemberType) => {
